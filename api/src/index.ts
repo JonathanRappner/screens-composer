1d4b20d447ce71e019eeba6360d4bafdf1667bff
Jonathan Rappner <jrappner@gmail.com>
2026-07-07 15:31:49 +0200

    load test data from db

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,20 @@
 import { serve } from "bun"
+import "dotenv/config"
+import mariadb from "mariadb"
+
+const pool = mariadb.createPool({
+    host: process.env.DB_HOST, 
+    user: process.env.DB_USER, 
+	port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
+    password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME,
+    connectionLimit: 5
+})
 
 serve({
 	port: 3000,
 
-	fetch(req) {
+	async fetch(req) {
 		const origin = req.headers.get("origin") || "*"
 
 		// Handle preflight request
@@ -17,7 +28,20 @@ serve({
 			})
 		}
 
-		return new Response("Hello from Bun!", {
+		// get data from db
+		let totalScreens: number = 0
+		try {
+			const conn = await pool.getConnection()
+			const rows = await conn.query("SELECT COUNT(*) AS count FROM screens")
+			totalScreens = rows[0].count
+			conn.end()
+		} catch(err) {
+			console.error("Error connecting to the database:", err)
+		}
+
+		const responseString = `Hello from the API! Total screens: ${totalScreens}`
+		console.log(responseString)
+		return new Response(responseString, {
 			headers: {
 				"Access-Control-Allow-Origin": origin,
 			},
